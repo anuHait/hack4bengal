24b5ec47040d310564b57edbf4428b5aa6c518a4
tannisthaneogy <tannisthaneogy1@gmail.com>
2023-07-09 17:04:14 +0530

    download page done

--- h4b/src/components/Navbar.jsx
+++ h4b/src/components/Navbar.jsx
@@ -6,10 +6,10 @@ import logo from "../assets/logo.png"
 const Navbar = () => {
   return (
     <div className=" flex flex-row justify-between items-center bg-cover bg-no-repeat bg-center overflow-hidden  mt-[0.5%] ml-[0.5%]  rounded-t-lg  "style={{ backgroundImage: `url(${grad})` }}>
-      <Link to='/' className='flex justify-center items-center gap-1'>
-      <img src={logo} alt="logo" className='w-auto p-3 pr-0 h-16 '/>
+    <Link to='/' className='flex justify-center items-center gap-1'>
+      <img src={logo} alt="logo" className='w-auto p-5 pr-0 h-20'/>
       <h1 className='font-semibold text-2xl font-poppins'>EDIC.<span className='text-primary'>AI</span></h1>
-      </Link>
+    </Link>
       <div className=' flex flex-row gap-1  lg:gap-4 mr-5 lg:mr-10'>
       <HashLink to="#service">
       <p className=' text-lg lg:text-xl'>Services</p>
--- h4b/src/pages/Download.jsx
+++ h4b/src/pages/Download.jsx
@@ -1,39 +1,41 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import grad from "../assets/grad.png";
-import hero from "../assets/hero-img.png"
+import hero from "../assets/downloadpg.jpg"
 import {ImDownload} from 'react-icons/im'
+import logo from "../assets/logo.png"
+
 const download = () => {
   return (
     <div>
-    <div className="flex flex-row justify-between items-center bg-cover bg-no-repeat bg-center w-[99%] mt-[0.5%] ml-[0.5%] overflow-hidden  rounded-t-lg " style={{ backgroundImage: `url(${grad})` }}>
-    <Link to='/'>
-      <h1 className='font-khand font-bold text-black text-4xl m-8'>MEDIC.<span className='text-primary'>AI</span></h1>
+    <div className="flex flex-row justify-between items-center bg-cover bg-no-repeat bg-center h-24 mt-[0.5%] ml-[0.5%] overflow-hidden rounded-t-lg bg-gradient-to-b from-purple-300">
+    <Link to='/' className='flex justify-center items-center gap-1'>
+      <img src={logo} alt="logo" className='w-auto p-5 pr-0 h-20'/>
+      <h1 className='font-bold text-2xl font-poppins'>EDIC.<span className='text-primary'>AI</span></h1>
     </Link>
-    <div className='flex flex-row gap-1  lg:gap-4 mr-5 lg:mr-10'>
+    <div className='flex flex-row gap-1 lg:gap-4 mr-5 lg:mr-10'>
       <Link to='/hospital'>
-        <p className='text-lg lg:text-xl'>Train Your Models</p>
+        <p className='text-lg text-gray-700 hover:text-black lg:text-lg font-poppins'>Train Your Models</p>
       </Link>
       <h2 className=' text-lg lg:text-xl'>/</h2>
       <Link to='/aggregate'>
-        <p className='text-lg lg:text-xl'>Aggregate</p>
+        <p className='text-lg text-gray-700 hover:text-black lg:text-lg font-poppins'>Aggregate</p>
       </Link>
     </div>
   </div>    
-  <div className=' m-10 lg:m-20 flex-col justify-center items-center'>
-  <h1 className='font-bold text-2xl lg:text-5xl'>Get MEDIC.AI for Desktop</h1>
-
+  <div className='flex flex-row gap-10 m-10 lg:m-20 justify-center items-center'>
+  <div>
+  <h1 className='font-bold text-2xl lg:text-5xl font-poppins'>Get MEDIC.AI for Desktop</h1>
   <div className='flex flex-col-reverse lg:flex-row gap-16 m-10'>
     <ul className='flex flex-col gap-3 lg:mr-10 list-decimal text-xl'>
-    <li>To start training your model,click below to download our application</li>
-    <li> After downloading it start the application by <span className='font-semibold'>Run as an administrator</span></li>
-    <button className='p-2 flex gap-3 bg-primary justify-center items-center mb-4 text-white font-semibold text-[1rem] lg:text-[1.4rem] rounded-lg w-[14rem]'>Download <ImDownload className='text-white text-lg'/></button>
-    <li>If you already have downloaded our application <Link to="/hospital" className='text-primary font-semibold'>click here</Link> to start training your model</li>
+      <li className='font-poppins text-lg'>To start training your model, click below to download our application.</li>
+      <button className='p-2 flex gap-3 bg-primary hover:bg-[#1A31CC] transition duration-200 justify-center items-center mb-4 text-white font-semibold text-[1rem] lg:text-[1.4rem] rounded-lg w-[14rem]'>Download <ImDownload className='text-white text-lg'/></button>
+      <li className='font-poppins text-lg'> After downloading it, start the application by <span className='font-semibold'>Run as an administrator.</span></li>
+      <li className='font-poppins text-lg'>If you have already downloaded our application <Link to="/hospital" className='text-primary font-semibold'>click here</Link> to start training your model.</li>
     </ul>
-  <img src={hero} alt='download' className='w-56'></img>
   </div>
-  
   </div>
+  <img src={hero} alt='download' className='w-[40%] mr-10 rounded-lg shadow-lg border-1 border-black'></img>  
+  </div>  
   </div>
   )
 }
