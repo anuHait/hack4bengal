c58b498b1827bffc78a8f712eb1dc4dcdaf7a43b
tannisthaneogy <tannisthaneogy1@gmail.com>
2023-07-09 17:28:38 +0530

    Navbar done

--- h4b/src/components/Navbar.jsx
+++ h4b/src/components/Navbar.jsx
@@ -5,18 +5,17 @@ import { Link } from 'react-router-dom';
 import logo from "../assets/logo.png"
 const Navbar = () => {
   return (
-    <div className=" flex flex-row justify-between items-center bg-cover bg-no-repeat bg-center overflow-hidden  mt-[0.5%] ml-[0.5%]  rounded-t-lg  "style={{ backgroundImage: `url(${grad})` }}>
-    <Link to='/' className='flex justify-center items-center gap-1'>
+    <div className="flex flex-row justify-between items-center bg-cover bg-no-repeat bg-center h-24 mt-[0.5%] ml-[0.5%] overflow-hidden rounded-t-lg bg-gradient-to-b from-purple-300">     <Link to='/' className='flex justify-center items-center gap-1'>
       <img src={logo} alt="logo" className='w-auto p-5 pr-0 h-20'/>
-      <h1 className='font-semibold text-2xl font-poppins'>EDIC.<span className='text-primary'>AI</span></h1>
+      <h1 className='font-bold text-2xl font-poppins'>EDIC.<span className='text-primary'>AI</span></h1>
     </Link>
       <div className=' flex flex-row gap-1  lg:gap-4 mr-5 lg:mr-10'>
       <HashLink to="#service">
-      <p className=' text-lg lg:text-xl'>Services</p>
+      <p className=' text-lg text-gray-700 hover:text-black lg:text-lg font-poppins'>Services</p>
       </HashLink>
         <h2 className=' text-lg lg:text-xl'>/</h2>
         <HashLink to="#aboutus">
-        <a href="/" className=' text-lg lg:text-xl'>About & Contact</a>
+        <a href="/" className=' text-lg text-gray-700 hover:text-black lg:text-lg font-poppins'>About & Contact</a>
         </HashLink>
       </div>
       </div>  
