f2dec0e309289c5414e80d0899b1df99cb8fbc9a
tannisthaneogy <tannisthaneogy1@gmail.com>
2023-07-10 13:23:18 +0530

    Service 3.0

--- h4b/src/components/Services.jsx
+++ h4b/src/components/Services.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import tick from "../assets/tickk.png";
 const Services = () => {
   return (
     <div className=" mt-10 " id="service">
@@ -7,7 +8,9 @@ const Services = () => {
       </h1>
       <div className="w-[60%] h-[1px] mx-auto bg-gray-800 mb-16"></div>
       <div className="flex flex-row gap-12 w-max mb-12 mx-auto lg:grid-cols-3 ">
-        <div className=" w-64 h-48 border-2 border-black bg-white rounded-2xl"></div>
+        <div className=" flex flex-row w-64 h-48 border-2 border-black bg-white rounded-2xl">
+          <img src={tick} alt="tick" className=" w-6 h-6 " />
+        </div>
         <div className=" w-64 h-48 border-2 border-black bg-white rounded-2xl"></div>
         <div className=" w-64 h-48 border-2 border-black bg-white rounded-2xl"></div>
       </div>
